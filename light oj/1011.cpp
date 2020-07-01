9a872b9fdd597b1b15897fbc0f2c455be5751b33
fahadmohad1 <fahad.mohad99@gmail.com>
2020-07-01 14:48:09 +0600

    added some light oj solution

---
+++ light oj/1011.cpp
@@ -0,0 +1,135 @@
+#include<bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+
+template <typename T>using ordered_set = tree<T, null_type, less<T>,rb_tree_tag, tree_order_statistics_node_update>;
+
+#define PI              acos(-1.0)
+#define en '\n'
+#define Infi 0x3f3f3f3f
+#define LInfi 0x3f3f3f3f3f3f3f3f
+
+#define setzero(a)    memset(a,0,sizeof(a))
+#define setneg(a)     memset(a,-1,sizeof(a))
+#define setinf(a)     memset(a,126,sizeof(a))
+
+
+#define READ_IN freopen("Input.txt","r",stdin);
+#define READ_OUT freopen("Output.txt","w",stdout);
+
+
+const int dx[] = {+1,-1,+0,+0};                         // 4 X directions
+const int dy[] = {+0,+0,+1,-1};                         // 4 Y directions
+const int dx8[] = {+0,+0,+1,-1,-1,+1,-1,+1};            // King X Moves
+const int dy8[] = {-1,+1,+0,+0,+1,+1,-1,-1};            // King Y Moves
+const int kx[] = {-2, -2, -1, -1,  1,  1,  2,  2};      // Knight X Moves
+const int ky[] = {-1,  1, -2,  2, -2,  2, -1,  1};      // Knight Y Moves
+
+
+
+int  Set    (int N,int pos) {return N=N | (1<<pos);}    /// turn a bit on
+int  Reset  (int N,int pos) {return N= N & ~(1<<pos);}  /// turn a bit off
+bool check  (int N,int pos) {return (bool)(N & (1<<pos));} /// check, if a bit is on or not
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define trace(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define trace(x...)
+#endif
+
+typedef long long int  ll;
+typedef unsigned long long int llu;
+
+vector<vector<int>> ar;
+int n;
+
+int dp[20][1<<16+2];
+int mask;
+int make(int id,int mask){
+
+
+    ///if(id>=ar.size())return 0;  /// we can use both the base case
+    if(mask==(1<<n)-1) return 0;
+
+    if(dp[id][mask]!=-1)return dp[id][mask];
+
+    int ans =0;
+
+    for(int i=0;i<n;i++){
+
+        if(check(mask,i)==0){
+
+            ans = max(ans,make(id+1,Set(mask,i)) + ar[id+1][i]);
+
+        }
+    }
+
+    return dp[id][mask] = ans;
+}
+
+int main(void){
+
+    int tt;
+    cin >> tt;
+
+    int kk=0;
+
+    while(tt--){
+        kk++;
+
+        cin >> n;
+
+
+        ar.assign(n,vector<int>(n,0));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                cin >> ar[i][j];
+            }
+        }
+
+        int ans = 0;
+
+        memset(dp,-1,sizeof(dp));
+
+         mask = 0;
+
+        for(int i=0;i<n;i++){
+
+            ans = max(ans,make(0,Set(mask,i)) + ar[0][i]);
+
+        }
+        cout << "Case " << kk  << ": ";
+        cout << ans << endl;
+
+    }
+
+
+
+    return 0;
+}
